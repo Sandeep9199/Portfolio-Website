cbba951efd9908addc4e9b05c47490b52b2dbd19
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-05-07 11:59:51 +0530

    Basic Portfolio Website

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["FrontEnd Developer ", "Backend Developer", "FullStack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed(".multiple-text", {
+  strings: ["FrontEnd Developer ", "Backend Developer", "FullStack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
